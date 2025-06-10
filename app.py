d7122b02128895b3ae0b236a678079955b01bf86
NguyenThiThuy <nt3thuy0504@gmail.com>
2025-06-10 12:52:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -195,6 +195,7 @@ if uploaded_file is not None:
         img_resized = img.resize((224, 224))
         img_array = image.img_to_array(img_resized)
         img_array = np.expand_dims(img_array, axis=0)
+        img_array = img_array.astype('float32')
        
 
         with st.spinner("🔍 Đang phân tích ảnh..."):
